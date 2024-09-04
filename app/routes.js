4932ee66d3a2caad8efca0a78d1c766dfeb888b4
mdalynomensa <mdaly@nomensa.com>
2024-09-04 13:04:36 +0100

    v28 created
    new version created

--- app/routes.js
+++ app/routes.js
@@ -38,4 +38,7 @@ require('./routes/24/routes.js')(router);
 require('./routes/25/routes.js')(router);
 require('./routes/26/routes.js')(router);
 require('./routes/27/routes.js')(router);
+require('./routes/28/routes.js')(router);
+
+
 
